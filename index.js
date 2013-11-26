ca3e140fcd8fbe3c0b7532c576f3f1a58f5f1113
streetlight <nweingartner@gmail.com>
2013-11-25 20:35:54 -0500

    Add Mt.Gox

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 var https = require('https'),
 	coinbase = require("coinbase-api")(),
+	MtGox = require('mtgox'),
+	gox = new MtGox(),
 	Twit = require('twit'),
 	below = 800;
 
 var getPrice = function() {
+
+	// Check Coinbase
 	coinbase.prices.spotRate(function(err, json) {
 
 		if (err) return console.log(err);
@@ -13,9 +17,21 @@ var getPrice = function() {
 		var price = json;
 
 		if (json.amount < below) {
-			console.log(json.amount);
+			console.log("Coinbase: " + json.amount);
 		}
+	});
+
+	// Check Mt.Gox
+	gox.market('BTCUSD', function(err, json) {
+		if (err) return console.log(err);
+
+		var mtGoxPrice = json;
+
+		if (json.last < below) {
+			console.log("Mt. Gox: " + json.last)
+		}
+
 	});
 }
 
-setInterval(getPrice, 5000);
+setInterval(getPrice, 1000);
